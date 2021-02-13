fde67eb801bcde2d9615a4fcaf5847b277087b2e
Phongpisut Meemuk <me.phongpisut_st@tni.ac.th>
2021-02-13 15:54:15 +0700

    0.1

---
+++ GameStore/Form1.Designer.vb
@@ -0,0 +1,235 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.lb_name = New System.Windows.Forms.Label()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Panel6 = New System.Windows.Forms.Panel()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Panel5 = New System.Windows.Forms.Panel()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Panel4 = New System.Windows.Forms.Panel()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TableLayoutPanel1.SuspendLayout()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.Panel6.SuspendLayout()
+        Me.Panel5.SuspendLayout()
+        Me.Panel4.SuspendLayout()
+        Me.Panel3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.BackColor = System.Drawing.Color.Transparent
+        Me.TableLayoutPanel1.ColumnCount = 1
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.Controls.Add(Me.Panel1, 0, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.Panel2, 0, 1)
+        Me.TableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(0, 0)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 2
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 26.36364!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 73.63636!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(435, 489)
+        Me.TableLayoutPanel1.TabIndex = 0
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.Gold
+        Me.Panel1.Controls.Add(Me.lb_name)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel1.Location = New System.Drawing.Point(3, 3)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(429, 122)
+        Me.Panel1.TabIndex = 0
+        '
+        'lb_name
+        '
+        Me.lb_name.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.lb_name.Font = New System.Drawing.Font("Segoe UI Semibold", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.lb_name.Location = New System.Drawing.Point(0, 0)
+        Me.lb_name.Name = "lb_name"
+        Me.lb_name.Size = New System.Drawing.Size(429, 122)
+        Me.lb_name.TabIndex = 0
+        Me.lb_name.Text = "Game Store"
+        Me.lb_name.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Panel2
+        '
+        Me.Panel2.Controls.Add(Me.Panel6)
+        Me.Panel2.Controls.Add(Me.Panel5)
+        Me.Panel2.Controls.Add(Me.Label2)
+        Me.Panel2.Controls.Add(Me.Panel4)
+        Me.Panel2.Controls.Add(Me.Panel3)
+        Me.Panel2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel2.Location = New System.Drawing.Point(3, 131)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Padding = New System.Windows.Forms.Padding(20)
+        Me.Panel2.Size = New System.Drawing.Size(429, 355)
+        Me.Panel2.TabIndex = 1
+        '
+        'Panel6
+        '
+        Me.Panel6.Controls.Add(Me.Button2)
+        Me.Panel6.Controls.Add(Me.Button1)
+        Me.Panel6.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel6.Location = New System.Drawing.Point(20, 220)
+        Me.Panel6.Name = "Panel6"
+        Me.Panel6.Padding = New System.Windows.Forms.Padding(50)
+        Me.Panel6.Size = New System.Drawing.Size(389, 116)
+        Me.Panel6.TabIndex = 4
+        '
+        'Button2
+        '
+        Me.Button2.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.Button2.Location = New System.Drawing.Point(50, 20)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(289, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "LOGIN"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.Button1.Location = New System.Drawing.Point(50, 43)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(289, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "REGISTER"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Panel5
+        '
+        Me.Panel5.Controls.Add(Me.TextBox2)
+        Me.Panel5.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel5.Location = New System.Drawing.Point(20, 159)
+        Me.Panel5.Name = "Panel5"
+        Me.Panel5.Padding = New System.Windows.Forms.Padding(20)
+        Me.Panel5.Size = New System.Drawing.Size(389, 61)
+        Me.Panel5.TabIndex = 3
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TextBox2.Location = New System.Drawing.Point(20, 20)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.TextBox2.Size = New System.Drawing.Size(349, 23)
+        Me.TextBox2.TabIndex = 0
+        '
+        'Label2
+        '
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label2.Location = New System.Drawing.Point(20, 119)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(389, 40)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Password"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.BottomLeft
+        '
+        'Panel4
+        '
+        Me.Panel4.Controls.Add(Me.TextBox1)
+        Me.Panel4.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel4.Location = New System.Drawing.Point(20, 60)
+        Me.Panel4.Name = "Panel4"
+        Me.Panel4.Padding = New System.Windows.Forms.Padding(20)
+        Me.Panel4.Size = New System.Drawing.Size(389, 59)
+        Me.Panel4.TabIndex = 1
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TextBox1.Location = New System.Drawing.Point(20, 20)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(349, 23)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Panel3
+        '
+        Me.Panel3.Controls.Add(Me.Label1)
+        Me.Panel3.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel3.Location = New System.Drawing.Point(20, 20)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(389, 40)
+        Me.Panel3.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(0, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(389, 40)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Username"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.BottomLeft
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(435, 489)
+        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel6.ResumeLayout(False)
+        Me.Panel5.ResumeLayout(False)
+        Me.Panel5.PerformLayout()
+        Me.Panel4.ResumeLayout(False)
+        Me.Panel4.PerformLayout()
+        Me.Panel3.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents lb_name As Label
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Panel4 As Panel
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents Panel5 As Panel
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Panel6 As Panel
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+End Class
---
+++ GameStore/Form1.vb
@@ -0,0 +1,147 @@
+﻿Imports System.Data.SQLite
+Imports System.Security.Cryptography
+Imports System.Text
+
+Public Class Form1
+    Dim dbpath = Application.StartupPath
+    Dim dbname = "data.db"
+    Dim constr As String = String.Format("Data Source = {0}", System.IO.Path.Combine(dbpath, dbname))
+
+
+    Public Function MD5(ByVal strString As String) As String
+        Dim ASCIIenc As New ASCIIEncoding
+        Dim strReturn As String
+        Dim ByteSourceText() As Byte = ASCIIenc.GetBytes(strString)
+        Dim Md5Hash As New MD5CryptoServiceProvider
+        Dim ByteHash() As Byte = Md5Hash.ComputeHash(ByteSourceText)
+        For Each b As Byte In ByteHash
+            strReturn = strReturn & b.ToString("x2")
+        Next
+        Return strReturn
+    End Function
+
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        Dim conn As New SQLiteConnection(constr)
+        conn.Open()
+
+        Try
+            Dim sql As String = "select * from Users Where Username = @name And Password = @password"
+            Dim cmd As New SQLiteCommand(sql, conn)
+            cmd.Parameters.AddWithValue("name", TextBox1.Text)
+            cmd.Parameters.AddWithValue("password", MD5(TextBox2.Text))
+            Dim reader As SQLiteDataReader = cmd.ExecuteReader
+
+            If Not reader.HasRows Then
+                MessageBox.Show("Invalid Username or Password", "Warning!", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            End If
+
+
+            While reader.Read
+                If reader(3) = 0 Then
+                    ''Guest
+
+                ElseIf reader(3) = 1 Then
+                    ''Admin
+
+
+                End If
+
+
+            End While
+
+
+        Catch ex As Exception
+            MessageBox.Show("Connection Lost!")
+
+        End Try
+
+
+
+
+
+        conn.Close()
+    End Sub
+
+    Private Sub TextBox1_KeyDown(sender As Object, e As KeyEventArgs) Handles TextBox1.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            TextBox2.Focus()
+        End If
+
+    End Sub
+
+    Private Sub TextBox2_KeyDown(sender As Object, e As KeyEventArgs) Handles TextBox2.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            Button2.PerformClick()
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim conn As New SQLiteConnection(constr)
+        conn.Open()
+
+        Try
+            Dim sql As String = "select * from Users Where Username = @name"
+            Dim cmd As New SQLiteCommand(sql, conn)
+            cmd.Parameters.AddWithValue("name", TextBox1.Text)
+
+            Dim reader As SQLiteDataReader = cmd.ExecuteReader
+
+            If reader.HasRows Then
+                MessageBox.Show("This Username has already used!", "Warning!", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Else
+                If TextBox1.Text.Length > 2 Then
+                    If TextBox2.Text.Length > 5 Then
+                        Dim x = 0
+                        For Each A In TextBox2.Text
+                            If IsNumeric(A) Then
+                                x += 1
+                            End If
+                        Next
+                        If x > 0 Then
+                            MessageBox.Show("Registered")
+                            sql = "insert into Users (Username,Password,Auth) values (@username,@password,0)"
+                            Dim cmd2 As New SQLiteCommand(sql, conn)
+                            cmd2.Parameters.AddWithValue("username", TextBox1.Text)
+                            cmd2.Parameters.AddWithValue("password", MD5(TextBox2.Text))
+                            cmd2.ExecuteNonQuery()
+
+
+                        Else
+                            MessageBox.Show("Password must contain number at least 1 character ", "Warning!", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+
+                        End If
+
+                    Else
+
+                        MessageBox.Show("Password require at least 6 character", "Warning!", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+
+                    End If
+                Else
+                    MessageBox.Show("Username require at least 3 character", "Warning!", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+                End If
+
+            End If
+
+
+
+        Catch ex As Exception
+            MessageBox.Show("Connection Lost!")
+
+        End Try
+
+
+
+
+
+        conn.Close()
+
+    End Sub
+End Class
